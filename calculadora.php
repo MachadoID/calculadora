d8f66478d733e2d51edebe712b0a8f3bdf84cdb0
Rafael Machado <101647305+rMachadoID@users.noreply.github.com>
2023-01-08 14:16:03 -0300

    Fix: trocando if e else if por switch e tratando a entrada de dados

--- calculadora.php
+++ calculadora.php
@@ -1,26 +1,29 @@
 <?php
 
-    include ("./index.php");
+include("index.php");
 
-    if($_POST){
-        $valor1 = $_POST ['texto-valor1'];
-        $valor2 = $_POST ['texto-valor2'];
-        $operacao = $_POST['operacao'];
-        $resultado = [];
+$valor1 = isset($_POST['texto-valor1']) && is_numeric($_POST['texto-valor1']) ? $_POST['texto-valor1'] : 0;
+$valor2 = isset($_POST['texto-valor2']) && is_numeric($_POST['texto-valor2']) ? $_POST['texto-valor2'] : 0;
 
-        if ($operacao == 'Soma'){
-            $resultado = $valor1 + $valor2;
-            echo "<center><br><h3>O resultado é: $resultado<h3></center><br>";
-        } else if ($operacao == 'Subtração'){
-            $resultado = $valor1 - $valor2;
-            echo "<center><br><h3>O resultado é: $resultado<h3></center><br>";
-        } else if ($operacao == 'Multiplicação'){
-            $resultado = $valor1 * $valor2;
-            echo "<center><br><h3>O resultado é: $resultado<h3></center><br>";
-        } else if ($operacao == 'Divisão'){
-            $resultado = $valor1 / $valor2;
-            echo "<center><br><h3>O resultado é: $resultado<h3></center><br>";
-        }
-    }
+$operacao = isset($_POST['operacao']) ? $_POST['operacao'] : null;
 
-?>
+switch ($operacao) {
+    case 'Soma':
+        $resultado = $valor1 + $valor2;
+        echo "<center><br><h3>O resultado é: $resultado<h3></center><br>";
+        break;
+    case 'Subtração':
+        $resultado = $valor1 - $valor2;
+        echo "<center><br><h3>O resultado é: $resultado<h3></center><br>";
+        break;
+    case 'Multiplicação':
+        $resultado = $valor1 * $valor2;
+        echo "<center><br><h3>O resultado é: $resultado<h3></center><br>";
+        break;
+    case 'Divisão':
+        $resultado = $valor1 / $valor2;
+        echo "<center><br><h3>O resultado é: $resultado<h3></center><br>";
+        break;
+    default:
+        echo "<center><br><h3>Escolha um operador<h3></center><br>";
+}
